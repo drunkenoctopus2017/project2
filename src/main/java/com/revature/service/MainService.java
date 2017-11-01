f23d509354e49461c538b0c003d2cd68f9abe93a
Benjamin Rogers <brogers0101@gmail.com>
2017-11-01 17:01:11 -0400

    accidentally deleted develop branch, but recovered, sorry

---
+++ src/main/java/com/revature/service/MainService.java
@@ -0,0 +1,39 @@
+package com.revature.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.dao.DAO;
+import com.revature.model.ScrumBoard;
+import com.revature.model.ScrumUser;
+
+@Service(value="MainService") //will be applied as a bean, and used with the transactionManager when needed
+@Transactional
+public class MainService {
+
+	@Autowired
+	private DAO dao;
+	
+	public ScrumUser getScrumUserByUsernameAndPassword(ScrumUser su) {
+		ScrumUser user = dao.getScrumUserByUsername(su);
+		if(su.getUsername().equals(user.getUsername()) && su.getPassword().equals(su.getPassword())) {
+			return user;
+		}
+		return null;
+	}
+	
+	public ScrumBoard createNewScrumBoard(ScrumBoard sb, ScrumUser su) {
+		sb.setUserId(su.getId());
+		ArrayList<ScrumUser> initialUsers = new ArrayList<>();
+		initialUsers.add(su);
+		sb.setScrumUsers(initialUsers);
+		sb = dao.createNewScrumBoard(sb);
+		su.getScrumBoards().add(sb);
+		su = dao.updateScrumUser(su);
+		return sb;
+	}
+	
+}
