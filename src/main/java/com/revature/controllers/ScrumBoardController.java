7e13f06bf5200b2466a4810031cac84a68d28e1d
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-05 17:25:21 -0500

    conflict fixed?

--- src/main/java/com/revature/controllers/ScrumBoardController.java
+++ src/main/java/com/revature/controllers/ScrumBoardController.java
@@ -79,11 +79,6 @@ public class ScrumBoardController {
 		return new ResponseEntity<ScrumBoardStory>(story, HttpStatus.OK); //200
 	}
 	
-	@RequestMapping(value="/updateScrumBoardStory", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ScrumBoardStory> updateScrumBoardStory(@RequestBody ScrumBoardStory sbs){
-		ScrumBoardStory story = service.updateScrumBoardStory(sbs);
-		return new ResponseEntity<ScrumBoardStory>(story, HttpStatus.OK);
-	}
 	//Delete
 	
 	/**
